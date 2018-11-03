aadc412e6b000d410313c026a2586b066deddee0
Fadhlie Ikram <paliey87@yahoo.co.uk>
2018-11-04 00:44:33 +0800

    Add download_dod package

---
+++ download_dod.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+dodnum=$1
+dodpath=$2
+marker="dodnum"
+dod_url="www.getdod.com/&param=dodnum"
+
+
+if [ -z ${dod_url} ] || [ -z ${dodnum} ] || [ -z ${dodpath} ]; then
+  echo '[-] Error: Missing required parameter(s).' >&2
+  exit 1
+fi
+
+# Construct download url
+url=$(echo ${dod_url} | sed s/${marker}/${dodnum}/g)
+
+if [ $? -ne 0 ]; then
+  echo "[-] Error: Failed to construct download url." >&2
+  exit 1
+fi
+
+# Perform download
+echo "[+] Downloading..."
+echo "[+] wget -v ${url} -O ${dodpath}/${dodnum}.zip"
+#wget -v ${url} -O ${dodpath}/${dodnum}.zip
+
+if [ $? -ne 0 ]; then
+  echo "[-] Error: Failed to download from ${url}" >&2
+  exit 1
+fi
+
+echo "[+] Download complete."
